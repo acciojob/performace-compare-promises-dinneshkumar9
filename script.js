f48ffaa7179e0b5440fc18c58b2d5bd356ba2c0e
Dinesh kumar <dinneshkumar9@gmail.com>
2023-03-18 10:14:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ const apiUrls = [
 ];
 
 // You can write your code here
+// Function to fetch data from APIs using Promise.all() and Promise.any()
+async function fetchData() {
+  const startTimeAll = performance.now(); // Start time for Promise.all()
+  const allResults = await Promise.all(apiUrls.map(url => fetch(url)));
+  const endTimeAll = performance.now(); // End time for Promise.all()
+  const timeTakenAll = endTimeAll - startTimeAll; // Time taken for Promise.all()
+
+  const startTimeAny = performance.now(); // Start time for Promise.any()
+  const anyResult = await Promise.any(apiUrls.map(url => fetch(url)));
+  const endTimeAny = performance.now(); // End time for Promise.any()
+  const timeTakenAny = endTimeAny - startTimeAny; // Time taken for Promise.any()
+
+  // Display time taken for each method in the table
+  document.getElementById("output-all").textContent = timeTakenAll;
+  document.getElementById("output-any").textContent = timeTakenAny;
+}
+
+fetchData(); // Call the fetchData function to display the results in the table
